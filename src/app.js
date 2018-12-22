d5f492aba374212a76ef18c81b2690f1ab192002
Brendan Kellams <brendan.kellams@gmail.com>
2018-12-22 11:22:47 -0800

    Init commit

---
+++ src/app.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter, { history } from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { login, logout } from './actions/auth';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
+
+const store = configureStore();
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+};
+
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch(login(user.uid));
+    renderApp();
+    if (history.location.pathname === '/') {
+      history.push('/dashboard');
+    }
+  } else {
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
+  }
+});
+
+// class OldSyntax {
+//   constructor() {
+//     this.name = 'Mike';
+//     this.getGreeting = this.getGreeting.bind(this);
+//   }
+//   getGreeting() {
+//     return `Hi my name is ${this.name}`;
+//   }
+// }
+
+// const oldSyntax = new OldSyntax();
+// const getGreeting = oldSyntax.getGreeting;
+// console.log(getGreeting());
+
+// //----------------------
+
+// class NewSyntax {
+//   name = 'Jen';
+//   getGreeting = () => {
+//     return `Hi my name is ${this.name}`;
+//   }
+// }
+
+// const newSyntax = new NewSyntax();
+// const newGetGreeting = newSyntax.getGreeting;
+// console.log(newGetGreeting());
